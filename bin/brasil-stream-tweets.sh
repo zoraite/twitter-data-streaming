c25f28d033375dc7d664a38c9ff0cd6f255ccc29
Igo Brilhante <brilhanteigo@gmail.com>
2014-10-14 16:56:33 -0300

    igo

--- bin/brasil-stream-tweets.sh
+++ bin/brasil-stream-tweets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # BRASIL
-python stream-tweets.py "twitter-streaming/brasil/brasil-georef-tweets" "-73.817,-33.733,-28.850,16.800"
+python ../src/stream-tweets.py "georef-tweets" "-73.817,-33.733,-28.850,16.800"
